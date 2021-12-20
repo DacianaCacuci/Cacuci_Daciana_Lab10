d9e5f6250692f3bde73206c7b7950ffaf7f44980
Daciana Cacuci <c.daciana2000@gmail.com>
2021-12-20 17:34:35 +0200

    Add project files.

---
+++ Cacuci_Daciana_Lab10/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Cacuci_Daciana_Lab10.Data;
+using System.IO;
+
+namespace Cacuci_Daciana_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Cacuci_Daciana_Lab10/ListEntryPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Cacuci_Daciana_Lab10.Models;
+using Cacuci_Daciana_Lab10.Data;
+using Cacuci_Daciana_Lab10;
+using Android.Content.Res;
+
+
+
+namespace Cacuci_Daciana_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListEntryPage : ContentPage
+    {
+        public ListEntryPage()
+        {
+            InitializeComponent();
+
+        }
+            protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetShopListsAsync();
+        }
+        async void OnShopListAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = new ShopList()
+            });
+        }
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ListPage
+                {
+                    BindingContext = e.SelectedItem as ShopList
+                });
+            }
+        }
+    }
+}
+
+
+
+
